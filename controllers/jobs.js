6ffdf54fb0757d536e8d28f42a95aec4dd3b974b
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-11-15 15:26:55 +0000

    .

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,44 @@
+const { NotFoundError } = require('../errors')
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    const jobs = await Job.find({ createdBy: req.user.userId })
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-    res.send('get job')
+    // ★ UPDATED: find → findOne
+    const job = await Job.findOne({ _id: req.params.id })   // ← updated
+    if (!job) throw new NotFoundError('not found')
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job')
+    const job = await Job.findOneAndUpdate(
+        {_id: req.params.id }, 
+        req.body, 
+        {new: true, runValidators: true}
+    )
+    if (!job) throw new NotFoundError('not found')
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    // ★ UPDATED: find → findOne
+    const job = await Job.findOne({ _id: req.params.id })   // ← updated
+    if (!job) throw new NotFoundError('not found')
+
+    // ★ UPDATED: deleteOne works on a doc, not an array
+    await job.deleteOne()                                   // ← stays correct
+
+    res.status(StatusCodes.NO_CONTENT).send()
 }
 
 module.exports = {
