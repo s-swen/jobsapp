90a8365dec9ab2787127ac1cfe89f4b051898887
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-11-16 11:41:49 +0000

    .

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ const app = express();
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 const authenticateUser = require('./middleware/authentication')
+
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}))
+app.use(helmet())
+app.use(xss())
+app.use(cors())
+
 app.use(express.json());
 // extra packages
+app.get('/', (req, res) => {
+  res.send('Jobs API')
+})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
